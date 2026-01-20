107163d564f8a2eab00fc55106da79e73eb23c01
Kendo <s6701012610376@email.kmutnb.ac.th>
2026-01-20 23:43:40 +0700

    chapter 6

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,43 +1,33 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
-from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
 FFoptions=Options()
 FFservice=Service(executable_path="/snap/bin/geckodriver")
-MAX_WAIT = 5
 
-class NewVisitorTest(unittest.TestCase):  
+
+class NewVisitorTest(LiveServerTestCase):  
     def setUp(self):  
         self.browser = webdriver.Firefox(options=FFoptions,service=FFservice)
 
     def tearDown(self):  
         self.browser.quit()
-        
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:  
-            try:
-                table = self.browser.find_element(By.ID, "id_nothing")
-                rows = table.find_elements(By.TAG_NAME, "tr")
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException):  
-                if time.time() - start_time > MAX_WAIT:  
-                    raise  
-                time.sleep(0.5)  
 
-
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
 
 
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -55,7 +45,8 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
@@ -63,17 +54,17 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # Satisfied, she goes back to sleep
 
         self.fail("Finish the test!")
 
         # The page updates again, and now shows both items on her list
-
 
         [...]
 
